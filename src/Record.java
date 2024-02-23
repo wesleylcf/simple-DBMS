7a6b435c96e11993f57d155d584e7f67214bd775
wesley lim <wesleylim.work@gmail.com>
2024-02-23 15:04:58 +0800

    feat(Record): Support conversion of Record to Bytes and Bytes to Record

--- src/Record.java
+++ src/Record.java
@@ -1,4 +1,11 @@
+import java.nio.ByteBuffer;
+
 public class Record {
+  public static final Integer RECORD_BYTE_SIZE = 20;
+  public static final Integer UUID_BYTE_SIZE = 10;
+  public static final Integer RATING_BYTE_SIZE = 4;
+  public static final Integer VOTES_BYTE_SIZE = 4;
+
   private String uuid;
   private Float averageRating;
   private Integer numVotes;
@@ -12,6 +19,47 @@ public class Record {
     this.numVotes = numVotes;
   }
 
+  /**
+   * Returns Byte[] of the current record
+   */
+  public byte[] marshal() {
+    ByteBuffer buffer = ByteBuffer.allocate(RECORD_BYTE_SIZE); // Total size including padding bytes
+
+    byte[] uuidBytes = new byte[UUID_BYTE_SIZE];
+    uuidBytes = uuid.getBytes();
+    buffer.put(uuidBytes);
+
+    buffer.put(padding1);
+    buffer.put(padding2);
+
+    // Put float and int values into the buffer
+    buffer.putFloat(averageRating);
+    buffer.putInt(numVotes);
+
+    return buffer.array();
+}
+
+/**
+ * Converts a Byte[] back to a Record
+ */
+public static Record unmarshal(byte[] bytes) {
+    ByteBuffer buffer = ByteBuffer.wrap(bytes);
+
+    byte[] uuidBytes = new byte[UUID_BYTE_SIZE + 2]; // include 2 bytes of padding
+    buffer.get(uuidBytes);
+    String uuid = new String(uuidBytes);
+
+    float averageRating = buffer.getFloat();
+    int numVotes = buffer.getInt();
+
+    return new Record(uuid, averageRating, numVotes);
+  }
+
+  @Override
+  public String toString() {
+      return String.format("{ uuid:'%s', averageRating:%f, numVotes:%d }", this.uuid, this.averageRating, this.numVotes);
+  }
+
   public String getUuid() {
     return uuid;
   }
@@ -22,10 +70,5 @@ public class Record {
 
   public Integer getNumVotes() {
     return numVotes;
-  }
-
-  @Override
-  public String toString() {
-      return String.format("{ uuid:'%s', averageRating:%f, numVotes:%d }", this.uuid, this.averageRating, this.numVotes);
   }
 }
