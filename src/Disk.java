1f849d106d4d819eaf64181214bf68b58e42b06c
wesley lim <wesleylim.work@gmail.com>
2024-02-26 15:47:24 +0800

    feat(Disk): Add a disk class since index should directly reference disk, and blocks are logical units only

---
+++ src/Disk.java
@@ -0,0 +1,28 @@
+import java.nio.ByteBuffer;
+
+class Disk {
+  public static int DISK_BYTE_SIZE = 500 * (int) Math.pow(2, 20);
+  private int blockSizeInBytes;
+  private ByteBuffer bytes;
+  private int occupiedBlocks = 0;
+
+  public Disk(int blockSizeInBytes) {
+    this.blockSizeInBytes = blockSizeInBytes;
+    this.bytes = ByteBuffer.allocate(DISK_BYTE_SIZE); // 500 MB;
+  }
+
+  public Block getBlock(int blockNumber) {
+    byte[] blockBytes = new byte[blockSizeInBytes];
+    bytes.position(getPositionFromBlockNumber(blockNumber)).get(blockBytes);
+    return new Block(blockBytes);
+  }
+
+  public void writeBlock(int blockNumber, Block block) {
+    bytes.position(getPositionFromBlockNumber(blockNumber));
+    bytes.put(block.marshal());
+  }
+
+  private int getPositionFromBlockNumber(int blockNumber) {
+    return (blockNumber-1) * blockSizeInBytes;
+  }
+}
