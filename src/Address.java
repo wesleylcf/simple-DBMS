e6646b745ef92afed12cb401786cfe81760c2919
jiantang99 <jiantang99@yahoo.com.sg>
2024-03-02 18:56:10 +0800

    Finished parent node and node, started on leafnode

---
+++ src/Address.java
@@ -0,0 +1,25 @@
+public class Address {
+    int id; // Block ID
+    int offset;
+
+    public Address(int id, int off){
+        this.id=blockId;
+        this.offset=off;
+    }
+
+    public int returnId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+}
--- src/Node.java
+++ src/Node.java
@@ -4,23 +4,21 @@ public class Node {
     
     private ArrayList<Integer> keys;
     private ArrayList<Integer> children;
-    // private ParentNode parent;
+    private ArrayList<Address> records;
     private Node parent;
+    private Node next;  // For leafnode
     private boolean leaf;
     private boolean root;
+    private boolean isParent;
     private String label;
 
-    public Node() {
-        keys = new ArrayList<Integer>();
-        leaf = false;
-        root = false;
-    }
-
-    /**
-     * Obtain key of given index
-     */
-    public int returnKey(int ind) {
-        return keys.get(ind);
+    public Node(boolean isParent = false, boolean isLeaf = false) {
+        this.keys = new ArrayList<Integer>();
+        this.leaf = isLeaf;
+        this.root = false;
+        this.isParent = isParent;
+        if(isParent){this.children = new ArrayList<Node>();}
+        if(isLeaf){records = new ArrayList<Address>();}
     }
 
     /**
@@ -30,61 +28,46 @@ public class Node {
 
         int len = this.returnKeys().size();
 
-        if (len == 0) {
+        if (len != 0) {
+            int i;
+            keys.add(key);
+            for (i = keys.size() -2; i >= 0; i--) {
+
+                if (keys.get(i) <= key) {
+
+                    i++;
+                    keys.set(i, key);
+                    break;
+                }
+
+                keys.set(i+1, keys.get(i));
+                if (i == 0) {
+
+                    keys.set(i, key);
+                    break;
+                }
+            }
+            return i;
+        }else{
             this.keys.add(key);
             return 0;
         }
-
-        int i;
-        keys.add(key);
-        for (i = keys.size() -2; i >= 0; i--) {
-
-            if (keys.get(i) <= key) {
-
-                i++;
-                keys.set(i, key);
-                break;
-            }
-
-            keys.set(i+1, keys.get(i));
-            if (i == 0) {
-
-                keys.set(i, key);
-                break;
-            }
-        }
-        
-        return i;
     }
 
     /**
-     * Remove key from specified index
+     * Return smallest key. Mostly used by the parent node, but is inside this class in case of 1st level parents
      */
-    public void returnKey(int ind) {
-
-        keys.remove(ind);
-    }
-
-    /**
-     * Removes all keys prior to splitting
-     */
-    public void returnKeys() {
-
-        keys = new ArrayList<Integer>();
-    }
-
-    // find smallest key (more for use by parentnode but placed here for first level of parents)
-    public int findSmallestKey() {
+    public int returnSmallest() {
 
         int key;
         ParentNode copy;
-        boolean isLeaf = this.getLeaf();
+        boolean isLeaf = this.returnLeaf();
 
         if (!isLeaf) {
 
             copy = (ParentNode) this;
 
-            while (!copy.getChild(0).getleaf())
+            while (!copy.getChild(0).returnLeaf())
                 copy = (ParentNode) copy.getChild(0);
             
             key = copy.getChild(0).returnKey(0);
@@ -96,69 +79,148 @@ public class Node {
         return key;
     }
 
-    // delete the node
-    public void deleteNode() {
+    /**
+     * Removes node
+     */
+    public void removeNode() {
 
         if (parent != null) {
 
-            parent.deleteChild(this);
+            parent.removeChild(this);
             parent = null;
         }
 
         if (this.leaf) {
-            
             LeafNode copy = (LeafNode) this;
-            copy.deleteRecords();
+            copy.records = new ArrayList<Address>();
             copy.setNext(null);
         }
-
         else {
-
-            ParentNode copy = (ParentNode) this;
-            copy.deleteChildren();
+            if(this.isParent == false){System.out.println("Node needs ot be a parent.")}
+            else{
+                Node copy = this;
+                copy.children = new ArrayList<Node>();
+            }
         }
 
-        leaf = false;
-        root = false;
+        this.leaf = false;
+        this.root = false;
         keys = new ArrayList<Integer>();
     }
 
-    // abstract void logStructure();
+    /**
+     * Appends child node to arraylist of children
+     */
+    public void appendChild(Node child){
+        int size = this.children.size();
+
+        if(size != 0){
+            int k = child.returnSmallest();
+            int s = this.returnSmallest();
+            int ind;
+
+            if (k >= s) {
+                this.children.add(this.addKey(k) +1, child);   
+            }
+            else {
+                ind = 0;
+                this.addKey(s);
+                this.children.add(0, child);
+            }
+
+            child.setParent(this);
+        }
+        else{
+            children.add(child);
+            child.setParent(this);
+        }
+    }
+
+    /**
+     * Appends child node to arraylist of children, at index 0
+     */
+    public void appendChild(Node child, int ind){
+        children.add(0, child);
+        child.setParent(this);
+        this.keys = new ArrayList<Integer>();
+        int size = children.size();
+        
+        for (int j = 0; j < size; j++) {
+            if (j != 0){
+                int smallest = children.get(j).returnSmallest();
+                addKey(smallest);
+            }
+        }
+    }
+
+    /**
+     * Remove specified child node
+     */
+    public void removeChild(Node child){
+        children.remove(child);
+        this.keys = new ArrayList<Integer>();
+        int size = this.children.size();
+        for (int j = 0; j < size; j++) {
+            if (j != 0){
+                int smallest = children.get(j).returnSmallest();
+                addKey(smallest);
+            }
+        }
+    }
+
+    /**
+     * Returns child node before
+     */
+    public Node returnChildBefore(Node node){
+        int ind = children.indexOf(node);
+        if (ind == 0){return null;}
+        else{return children.get(children.indexOf(node)-1)};
+    }
+
+    /**
+     * Returns child node after
+     */
+    public Node returnChildAfter(Node node){
+        int ind = children.indexOf(node);
+        int maxInd = children.size()-1;
+        if (ind == maxInd){return null;}
+        else{return children.get(children.indexOf(node)+1);}
+    }
 
     // Getters and Setters
 
     /**
      * Getter for leaf field
      */
-    public boolean getleaf() {
+    public boolean returnLeaf() {
         return this.leaf;
     }
 
     /**
      * Setter for leaf field
      */
-    public void setleaf(boolean leaf) {
+    public void setLeaf(boolean leaf) {
         this.leaf = leaf;
     }
 
     /**
      * Getter for root field
      */
-    public boolean getroot() {
+    public boolean returnRoot() {
         return this.root;
     }
 
     /**
      * Setter for root field
      */
-    public void setroot(boolean root) {
+    public void setRoot(boolean root) {
         root = root;
     }
 
     /**
      * Getter for parent field
      */
-    public ParentNode getParent() {
+    public ParentNode returnParent() {
         return this.parent;
     }
 
@@ -170,9 +232,58 @@ public class Node {
     }
 
     /**
+     * Obtain key of given index
+     */
+    public int returnKey(int ind) {
+        return keys.get(ind);
+    }
+
+    /**
      * Getter for keys field
      */
     public ArrayList<Integer> returnKeys() {
         return this.keys;
+    }
+
+    /**
+     * Getter for children field
+     */
+    public ArrayList<Node> returnChildren(){
+        return this.children;
+    }
+
+    /**
+     * Return child node of specified index
+     */
+    public Node returnChild(int ind){
+        return this.children.get(ind);
+    }
+
+    /**
+     * Returns records field
+     */
+    public ArrayList<Address> returnRecords(){
+        return this.records;
+    }
+
+    /**
+     * Returns record of specified index
+     */
+    public Address returnRecords(int ind){
+        return this.records.get(ind);
+    }
+
+    /**
+     * Returns next leaf node
+     */
+    public Node returnNext() {
+        return this.next;
+    }
+
+    /**
+     * Setter for next leafnode
+     */
+    public void setNext(Node node) {
+        this.next = node;
     }
 }
